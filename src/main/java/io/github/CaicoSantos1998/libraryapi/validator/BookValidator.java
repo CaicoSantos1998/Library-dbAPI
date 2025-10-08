23ed2a4d2a42aed37d0e7af57e76a14f65c65f66
CS <cs_1998@hotmail.com>
2025-10-08 11:46:15 -0300

    Entity responsible for the validation

--- src/main/java/io/github/CaicoSantos1998/libraryapi/validator/BookValidator.java
+++ src/main/java/io/github/CaicoSantos1998/libraryapi/validator/BookValidator.java
@@ -1,4 +1,44 @@
 package io.github.CaicoSantos1998.libraryapi.validator;
 
+import io.github.CaicoSantos1998.libraryapi.exceptions.FieldInvalidatedException;
+import io.github.CaicoSantos1998.libraryapi.exceptions.RegisterDuplicatedException;
+import io.github.CaicoSantos1998.libraryapi.model.Book;
+import io.github.CaicoSantos1998.libraryapi.repository.RepositoryBook;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component
+@RequiredArgsConstructor
 public class BookValidator {
+
+    private static final int YEAR_MANDATORY_PRICE = 2020;
+
+    private final RepositoryBook repository;
+
+    public void validate(Book book) {
+        if(existsBookWithIsbn(book)) {
+            throw new RegisterDuplicatedException("ISBN already registered!");
+        }
+        if(isPriceMandatoryNull(book)) {
+            throw new FieldInvalidatedException("price", "Books published after 2020 have a mandatory price!");
+        }
+    }
+
+    private boolean isPriceMandatoryNull(Book book) {
+        return book.getPrice() == null && book.getDatePublication().getYear() >= YEAR_MANDATORY_PRICE;
+    }
+
+    private boolean existsBookWithIsbn(Book book) {
+        Optional<Book> bookFound = repository.findByIsbn(book.getIsbn());
+
+        if(book.getId()==null) {
+            return bookFound.isPresent();
+        }
+        return bookFound
+                .map(Book::getId)
+                .stream()
+                .anyMatch(id -> !id.equals(book.getId()));
+    }
 }
--- src/main/java/io/github/CaicoSantos1998/libraryapi/validator/ValidatorAuthor.java
+++ src/main/java/io/github/CaicoSantos1998/libraryapi/validator/ValidatorAuthor.java
@@ -1,4 +1,40 @@
 package io.github.CaicoSantos1998.libraryapi.validator;
 
+import io.github.CaicoSantos1998.libraryapi.exceptions.RegisterDuplicatedException;
+import io.github.CaicoSantos1998.libraryapi.model.Author;
+import io.github.CaicoSantos1998.libraryapi.repository.RepositoryAuthor;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component
 public class ValidatorAuthor {
+
+    private final RepositoryAuthor repository;
+
+    public ValidatorAuthor(RepositoryAuthor repository) {
+        this.repository = repository;
+    }
+
+    public void validate(Author author) {
+        if(existsAuthor(author)) {
+            throw new RegisterDuplicatedException("Author already exists in the Database!");
+        }
+    }
+
+    private boolean existsAuthor(Author author) {
+        Optional<Author> authorFound = repository.findByNameAndDateBirthAndNationality(
+                author.getName(), author.getDateBirth(), author.getNationality()
+        );
+
+        if(author.getId()==null) {
+            return authorFound.isPresent();
+        }
+
+        if(authorFound.isEmpty()) {
+            return false;
+        }
+
+        return !author.getId().equals(authorFound.get().getId());
+    }
 }
