5bbf692d62046d13e5cb52129a42f674cbb3d585
CS <cs_1998@hotmail.com>
2025-10-08 16:00:01 -0300

    Layer reponsible for security and user authentication

--- src/main/java/io/github/CaicoSantos1998/libraryapi/config/SecurityConfiguration.java
+++ src/main/java/io/github/CaicoSantos1998/libraryapi/config/SecurityConfiguration.java
@@ -1,4 +1,58 @@
 package io.github.CaicoSantos1998.libraryapi.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
 public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain securityFilter(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .csrf(AbstractHttpConfigurer::disable)
+                .httpBasic(Customizer.withDefaults())
+                .formLogin(configurer -> {
+                    configurer.loginPage("/login").permitAll();
+                })
+                .authorizeHttpRequests(authorize -> {
+                    authorize.requestMatchers("/authors/**").hasRole("ADMIN");
+                    authorize.requestMatchers("/books/**").hasAnyRole("USER", "ADMIN");
+                    authorize.anyRequest().authenticated();
+                })
+                .build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(10);
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(PasswordEncoder encoder) {
+        UserDetails user1 = User.builder()
+                .username("user")
+                .password(encoder.encode("123"))
+                .roles("USER")
+                .build();
+
+        UserDetails user2 = User.builder()
+                .username("admin")
+                .password(encoder.encode("321"))
+                .roles("ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(user1, user2);
+    }
 }
