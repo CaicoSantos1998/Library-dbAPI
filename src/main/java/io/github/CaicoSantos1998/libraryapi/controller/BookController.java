bd1f25d79270b7da1439cfb86ec3f2ba9009d881
CS <cs_1998@hotmail.com>
2025-10-08 11:18:41 -0300

    Created entity book controller

--- src/main/java/io/github/CaicoSantos1998/libraryapi/controller/BookController.java
+++ src/main/java/io/github/CaicoSantos1998/libraryapi/controller/BookController.java
@@ -1,4 +1,95 @@
 package io.github.CaicoSantos1998.libraryapi.controller;
 
-public class BookController {
+import io.github.CaicoSantos1998.libraryapi.controller.dto.RegistrationBookDTO;
+import io.github.CaicoSantos1998.libraryapi.controller.dto.ResultSearchBookDTO;
+import io.github.CaicoSantos1998.libraryapi.controller.mappers.BookMapper;
+import io.github.CaicoSantos1998.libraryapi.model.Book;
+import io.github.CaicoSantos1998.libraryapi.model.GenderBook;
+import io.github.CaicoSantos1998.libraryapi.service.BookService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("books")
+@RequiredArgsConstructor
+public class BookController implements GenericController {
+
+    private final BookService service;
+    private final BookMapper mapper;
+
+    @PostMapping
+    public ResponseEntity<Void> save(@RequestBody @Valid RegistrationBookDTO rbDTO) {
+        Book book = mapper.toEntity(rbDTO);
+        service.save(book);
+        URI uri = generateHeaderLocation(book.getId());
+        return ResponseEntity.created(uri).build();
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<ResultSearchBookDTO> getDetails(@PathVariable String id) {
+        return service.getById(UUID.fromString(id))
+                .map(book -> {
+                    var BookDTO = mapper.toDTO(book);
+                    return ResponseEntity.ok(BookDTO);
+                }).orElseGet( () -> ResponseEntity.notFound().build() );
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<Object> delete(@PathVariable String id) {
+        return service.getById(UUID.fromString(id))
+                .map(book -> {
+                    service.delete(book);
+                    return ResponseEntity.noContent().build();
+                }).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<ResultSearchBookDTO>> search(
+            @RequestParam(value = "isbn", required = false)
+            String isbn,
+            @RequestParam(value = "title", required = false)
+            String title,
+            @RequestParam(value = "nameAuthor", required = false)
+            String nameAuthor,
+            @RequestParam(value = "gender", required = false)
+            GenderBook gender,
+            @RequestParam(value = "publicationYear", required = false)
+            Integer publicationYear,
+            @RequestParam(value = "page", defaultValue = "0")
+            Integer page,
+            @RequestParam(value = "pageSize", defaultValue = "10")
+            Integer pageSize
+            ) {
+        Page<Book> pageResult = service.search(
+                isbn, title, nameAuthor, gender, publicationYear, page, pageSize);
+
+        Page<ResultSearchBookDTO> result = pageResult.map(mapper::toDTO);
+
+        return ResponseEntity.ok(result);
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<Object> update(
+            @PathVariable String id,
+            @RequestBody @Valid RegistrationBookDTO dto) {
+        return service.getById(UUID.fromString(id))
+                .map(book -> {
+                    Book entityAux = mapper.toEntity(dto);
+                    book.setTitle(entityAux.getTitle());
+                    book.setDatePublication(entityAux.getDatePublication());
+                    book.setGender(entityAux.getGender());
+                    book.setPrice(entityAux.getPrice());
+                    book.setAuthor(entityAux.getAuthor());
+
+                    service.update(book);
+                    return ResponseEntity.noContent().build();
+                }).orElseGet( () -> ResponseEntity.notFound().build() );
+    }
 }
