de7849875f557d8e12ae89111d1d76a1385ce76b
CS <cs_1998@hotmail.com>
2025-10-09 15:45:37 -0300

    Updated

--- src/main/java/io/github/CaicoSantos1998/libraryapi/security/CustomUsersDetailsService.java
+++ src/main/java/io/github/CaicoSantos1998/libraryapi/security/CustomUsersDetailsService.java
@@ -1,4 +1,30 @@
 package io.github.CaicoSantos1998.libraryapi.security;
 
-public class CustomUsersDetailsService {
+import io.github.CaicoSantos1998.libraryapi.model.Users;
+import io.github.CaicoSantos1998.libraryapi.service.UsersService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+@RequiredArgsConstructor
+public class CustomUsersDetailsService implements UserDetailsService {
+
+    private final UsersService service;
+
+    @Override
+    public UserDetails loadUserByUsername(String login) throws UsernameNotFoundException {
+        Users users = service.getByLogin(login);
+
+        if(users==null) {
+            throw new UsernameNotFoundException("User not found!");
+        }
+
+        return User.builder()
+                .username(users.getLogin())
+                .password(users.getPassword())
+                .roles(users.getRoles().toArray(new String[users.getRoles().size()]))
+                .build();
+    }
 }
