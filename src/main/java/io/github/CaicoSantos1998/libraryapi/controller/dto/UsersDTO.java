2b871b08e201fe401757be76cee08bdc23d43d5e
CS <cs_1998@hotmail.com>
2025-10-09 15:32:36 -0300

    UsersDTO class created

--- src/main/java/io/github/CaicoSantos1998/libraryapi/controller/dto/UsersDTO.java
+++ src/main/java/io/github/CaicoSantos1998/libraryapi/controller/dto/UsersDTO.java
@@ -1,4 +1,6 @@
 package io.github.CaicoSantos1998.libraryapi.controller.dto;
 
-public class UsersDTO {
+import java.util.List;
+
+public record UsersDTO(String login, String password, List<String> roles) {
 }
