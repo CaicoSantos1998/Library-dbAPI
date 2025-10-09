3f1ae9107037f04c0860470f413e18e078ef40a6
CS <cs_1998@hotmail.com>
2025-10-09 15:48:01 -0300

    Class responsible for receiving user authentication using UserDetails to receive getPrincipal and finally get the logged in user

--- src/main/java/io/github/CaicoSantos1998/libraryapi/security/SecurityService.java
+++ src/main/java/io/github/CaicoSantos1998/libraryapi/security/SecurityService.java
@@ -1,4 +1,24 @@
 package io.github.CaicoSantos1998.libraryapi.security;
 
+import io.github.CaicoSantos1998.libraryapi.model.Users;
+import io.github.CaicoSantos1998.libraryapi.service.UsersService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
 public class SecurityService {
+
+    private final UsersService usersService;
+
+    public Users getUserLogged() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+        String login = userDetails.getUsername();
+        return usersService.getByLogin(login);
+    }
+
 }
