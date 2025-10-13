0d995580854bcde4b643a1b775c6e47c3b95ef07
CS <cs_1998@hotmail.com>
2025-10-13 15:18:15 -0300

    Little updating for method references

--- src/main/java/io/github/CaicoSantos1998/libraryapi/security/CustomAuthentication.java
+++ src/main/java/io/github/CaicoSantos1998/libraryapi/security/CustomAuthentication.java
@@ -21,7 +21,7 @@ public class CustomAuthentication implements Authentication {
         return this.users
                 .getRoles()
                 .stream()
-                .map(role -> new SimpleGrantedAuthority(role))
+                .map(SimpleGrantedAuthority::new)
                 .collect(Collectors.toList());
     }
 
