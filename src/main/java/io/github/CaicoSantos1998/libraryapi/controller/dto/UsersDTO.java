89f43b3595325de17a126f7e194b4a8114aa938d
CS <cs_1998@hotmail.com>
2025-10-14 13:49:36 -0300

    Field validations added

--- src/main/java/io/github/CaicoSantos1998/libraryapi/controller/dto/UsersDTO.java
+++ src/main/java/io/github/CaicoSantos1998/libraryapi/controller/dto/UsersDTO.java
@@ -1,6 +1,20 @@
 package io.github.CaicoSantos1998.libraryapi.controller.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
 import java.util.List;
 
-public record UsersDTO(String login, String password, List<String> roles) {
+public record UsersDTO(
+        @NotBlank(message = "Required field!")
+        String login,
+
+        @NotBlank(message = "Required field!")
+        String password,
+
+        @Email(message = "Is not a valid email!")
+        @NotBlank(message = "Required field!")
+        String email,
+
+        List<String> roles) {
 }
